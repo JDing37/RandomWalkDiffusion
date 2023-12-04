f96489a18e7c612e2166770b84ae1c5da1df8d1c
JDing37 <dingillo72@gmail.com>
2023-12-03 19:11:13 -0600

    Update

--- RwDiff.py
+++ RwDiff.py
@@ -1,1 +1,19 @@
-print("Hi, Anu :)")
+import random
+import matplotlib as plt
+from pylab import *
+
+class RandomWalk:
+
+    def one_D(self, N):
+        u = 2*randint(0,2,size=N)-1
+        x = cumsum(u)
+        return x
+
+
+
+rand = RandomWalk()
+x = rand.one_D(1000)
+N = list(range(1,1001))
+plt.plot(x, N)
+plt.savefig('x(N).png')
+        
